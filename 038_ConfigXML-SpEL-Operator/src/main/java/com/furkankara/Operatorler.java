b6aa9fcf253f5842b1021c48d356c22a623d6dfd
Furkan9528 <Furkan9528@github.com>
2020-04-09 11:35:00 +0200

    Example of ConfigXML-SpEL-Operator

---
+++ 038_ConfigXML-SpEL-Operator/src/main/java/com/furkankara/Operatorler.java
@@ -0,0 +1,133 @@
+package com.furkankara;
+
+public class Operatorler {
+
+		//1.iliskisel - Relational
+		private boolean equalTest;
+		private boolean notEqualTest;
+		private boolean lessThanTest;
+		private boolean lessThanOrEqualTest;
+		private boolean graterThanTest;
+		private boolean graterThanOrEqualTest;
+
+
+		//2.Mantiksal - Logical
+		private boolean andTest;
+		private boolean orTest;
+		private boolean noTest;
+
+		
+		//3.Matematik - Mathematical
+		private double addTest;//toplama
+		private String addStringTest;//topla sonuc string
+		private double substractionTest;//cikarma
+		private double multiplicationTest;//carpma
+		private double divisionTest;//bolme
+		private double modulesTest;//modul
+		private double exponentialTest; //uzeri ussu
+
+		
+		public boolean isEqualTest() {
+			return equalTest;
+		}
+		public void setEqualTest(boolean equalTest) {
+			this.equalTest = equalTest;
+		}
+		public boolean isNotEqualTest() {
+			return notEqualTest;
+		}
+		public void setNotEqualTest(boolean notEqualTest) {
+			this.notEqualTest = notEqualTest;
+		}
+		public boolean isLessThanTest() {
+			return lessThanTest;
+		}
+		public void setLessThanTest(boolean lessThanTest) {
+			this.lessThanTest = lessThanTest;
+		}
+		public boolean isGraterThanTest() {
+			return graterThanTest;
+		}
+		public void setGraterThanTest(boolean graterThanTest) {
+			this.graterThanTest = graterThanTest;
+		}
+		public boolean isGraterThanOrEqualTest() {
+			return graterThanOrEqualTest;
+		}
+		public void setGraterThanOrEqualTest(boolean graterThanOrEqualTest) {
+			this.graterThanOrEqualTest = graterThanOrEqualTest;
+		}
+		public boolean isAndTest() {
+			return andTest;
+		}
+		public void setAndTest(boolean andTest) {
+			this.andTest = andTest;
+		}
+		public boolean isOrTest() {
+			return orTest;
+		}
+		public void setOrTest(boolean orTest) {
+			this.orTest = orTest;
+		}
+		public boolean isNoTest() {
+			return noTest;
+		}
+		public void setNoTest(boolean noTest) {
+			this.noTest = noTest;
+		}
+		public double getAddTest() {
+			return addTest;
+		}
+		public void setAddTest(double addTest) {
+			this.addTest = addTest;
+		}
+		public String getAddStringTest() {
+			return addStringTest;
+		}
+		public void setAddStringTest(String addStringTest) {
+			this.addStringTest = addStringTest;
+		}
+		public double getSubstractionTest() {
+			return substractionTest;
+		}
+		public void setSubstractionTest(double substractionTest) {
+			this.substractionTest = substractionTest;
+		}
+		public double getMultiplicationTest() {
+			return multiplicationTest;
+		}
+		public void setMultiplicationTest(double multiplicationTest) {
+			this.multiplicationTest = multiplicationTest;
+		}
+		public double getDivisionTest() {
+			return divisionTest;
+		}
+		public void setDivisionTest(double divisionTest) {
+			this.divisionTest = divisionTest;
+		}
+		public double getModulesTest() {
+			return modulesTest;
+		}
+		public void setModulesTest(double modulesTest) {
+			this.modulesTest = modulesTest;
+		}
+		public double getExponentialTest() {
+			return exponentialTest;
+		}
+		public void setExponentialTest(double exponentialTest) {
+			this.exponentialTest = exponentialTest;
+		}
+		public boolean isLessThanOrEqualTest() {
+			return lessThanOrEqualTest;
+		}
+		public void setLessThanOrEqualTest(boolean lessThanOrEqualTest) {
+			this.lessThanOrEqualTest = lessThanOrEqualTest;
+		}
+
+
+		
+		
+
+		
+	
+}
